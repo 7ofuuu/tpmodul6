8cb865f2a63c9d19297b1e1841c41f2620691690
Mohammad Ilham Firdaus <ilhamfir09@gmail.com>
2025-03-24 00:17:26 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using tpmodul6_103022300043;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Mohammad Ilham Firdaus");
+        video.PrintVideoDetails();
+    }
+}
